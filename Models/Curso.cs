c497e7949aeeb3d3ddd3b556b3d8f556f9082f15
LucasF <lucas.feiteira@outlook.com>
2025-03-24 17:35:22 -0300

    .

---
+++ Models/Curso.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploExplorando.Models
+{
+    public class Curso
+    {
+        public string Nome { get; set; }
+        public List<Pessoa> Alunos { get; set; }
+
+        public void AdicionarAluno(Pessoa aluno)
+        {
+            Alunos.Add(aluno);
+        }
+
+        public int ObterQuantidadeDeAlunosMatriculados()
+        {
+            int quantidade = Alunos.Count;
+            return quantidade;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Lucas";
+p1.Sobrenome = "Henrique"; 
+p1.Idade = 25;
+p1.Apresentar();
+
