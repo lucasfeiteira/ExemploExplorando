1dbfffb8573e7798ca1abbb926b7d6958a950848
LucasF <lucas.feiteira@outlook.com>
2025-04-03 16:33:02 -0300

    .

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -30,7 +30,7 @@ namespace ExemploExplorando.Models
             System.Console.WriteLine($"Alunos do curso de: {Nome}");
             for (int count = 0; count < Alunos.Count; count++)
             {
-                string texto = $"N° {count} - {Alunos[count].NomeCompleto}";
+                string texto = $"N° {count + 1} - {Alunos[count].NomeCompleto}";
                 Console.WriteLine(texto);
             }
         }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,103 @@
 ﻿using ExemploExplorando.Models;
+using System.Globalization;
 
-Pessoa p1 = new Pessoa("Lucas", "Henrique");
-Pessoa p2 = new Pessoa("Eduardo", "Neves Queiroz");
+string dataString = "2025-04-03 18:00";
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Ingles";
-cursoDeIngles.Alunos = new List<Pessoa>();
+bool sucesso = DateTime.TryParseExact(dataString,
+                        "yyyy-MM-dd HH:mm",
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.None, out DateTime data);
 
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.ListarAlunos();
+if (sucesso)
+{
+    System.Console.WriteLine($"Conversao com sucesso! data {data}");
+}
+else
+{
+    System.Console.WriteLine($"{dataString} nao é data válida");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+// decimal valorMonetario = 1582.40M;
+
+// Console.WriteLine(valorMonetario.ToString("N2"));
+
+// double porcentagem = .3421;
+
+// System.Console.WriteLine(porcentagem.ToString("P"));
+
+// int numero = 123456;
+// System.Console.WriteLine(numero.ToString("##-##-##"));
+
+
+
+
+
+
+
+
+
+// int numero1 = 10;
+// int numero2 = 20;
+
+// int resultado = numero1 + numero2;
+
+// System.Console.WriteLine(resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa("Lucas", "Henrique");
+// Pessoa p2 = new Pessoa("Eduardo", "Neves Queiroz");
+
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Ingles";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.ListarAlunos();
 
 
 
